64038ef3fc7445cc70d9eb4c2d4aa10dd382f3dc
Joe Curran <joe.curran@motionlab.co.uk>
2018-01-26 14:41:04 +0000

    BH02 – Heading Logo Support
    Added support for adding a logo above the heading.

--- template-parts/newBlocks/heading.php
+++ template-parts/newBlocks/heading.php
@@ -1,6 +1,28 @@
-<!-- heading -->
+<?php
+/**
+ * HEADING BLOCK -----------------------------------
+ * Adds a customisable heading to the page and added
+ * support for adding a brand logo.
+ *
+ * @author Jason
+ * @created Unknown
+ * @updated 26 Jan 2018 (Joe Curran) – Added Heading Logo Field.
+ *
+ * @version 1.01
+ */
+
+ $logo = get_sub_field('heading_logo_basic_image');
+
+?>
+
 <section class="overflow-hidden <?php echo 'bg-' . get_sub_field('background_color')?> <?php echo $txtColor ?> <?php echo get_sub_field('animate_block') == TRUE ? 'overflow-hidden' : '' ?>">
     <div class="px4 lg-px0 <?php echo $paddingTop == 'collapse-top' ? 'pt0' : 'pt4 md-pt5' ?> pb4 md-pb5 || <?php echo get_sub_field('text_align_align') ?> <?php echo get_sub_field('full_width_full_width') ?> <?php echo $measureWide ?>" <?php echo $animationType ?> <?php echo $animationSpeed ?>  <?php echo $animationRepeat ?>  <?php echo $animationDelay ?> >
+
+        <?php if (!empty($logo)):?>
+            <div class="center pb3">
+                <img src="<?=$logo['url']?>" alt="<?=$logo['alt'];?>">
+            </div>
+        <?php endif; ?>
 
         <?php include(get_template_directory() .'/template-parts/newBlocks/sub-elements/title-loop.php') ?>
 
