f74cb0d90050d1f88f4801c4e3ff43f460c17474
Joe Curran <joe.curran@motionlab.co.uk>
2018-01-29 15:01:02 +0000

    Update Controller for New Blocks
    Make the block controller a little more slick for new blocks.

--- template-parts/building-blocks.php
+++ template-parts/building-blocks.php
@@ -2,62 +2,69 @@
 $index = 0;
 $previousBgColor = "";
 $blockNumber = 0;
+
 if( have_rows('building_blocks') ) {
     while ( have_rows('building_blocks') ) {
         the_row();
 
-        // Varibales for the Blocks
+        // Varibales for the Blocks – TODO: Needs looking over!
         include(get_template_directory() .'/inc/block-variables.php');
 
 
-        if ( get_row_layout() == 'heading'){
-            include(get_template_directory() .'/template-parts/newBlocks/heading.php');
-        } elseif( get_row_layout() == 'columns'){
-            include(get_template_directory() .'/template-parts/newBlocks/column_text.php');
-        } elseif( get_row_layout() == 'tabs_simple'){
-            include(get_template_directory() .'/template-parts/newBlocks/tabs_simple.php');
-        } elseif( get_row_layout() == 'full_width_banner'){
-            include(get_template_directory() .'/template-parts/newBlocks/full_width_banner.php');
-        } elseif( get_row_layout() == 'media_object'){
-            include(get_template_directory() .'/template-parts/newBlocks/media_object.php');
-        } elseif( get_row_layout() == 'slider_simple'){
-            include(get_template_directory() .'/template-parts/newBlocks/slider_simple.php');
-        } elseif( get_row_layout() == 'alternating_media'){
-            include(get_template_directory() .'/template-parts/newBlocks/alternating_media.php');
-        } elseif( get_row_layout() == 'gallery_simple'){
-            include(get_template_directory() .'/template-parts/newBlocks/gallery_simple.php');
-        } elseif( get_row_layout() == 'pods'){
-            include(get_template_directory() .'/template-parts/newBlocks/pods.php');
-        } elseif( get_row_layout() == 'form'){
-            include(get_template_directory() .'/template-parts/newBlocks/form.php');
-        } elseif( get_row_layout() == 'table'){
-            include(get_template_directory() .'/template-parts/newBlocks/table.php');
-        } elseif( get_row_layout() == 'spacer'){
-            include(get_template_directory() .'/template-parts/newBlocks/spacer.php');
-        } elseif( get_row_layout() == 'sticky_menu'){
-            include(get_template_directory() .'/template-parts/newBlocks/menu_sticky.php');
-        } elseif( get_row_layout() == 'sticky_menu_anchor'){
-            include(get_template_directory() .'/template-parts/newBlocks/menu_sticky_anchor.php');
-        }  elseif( get_row_layout() == 'logo_slider'){
-            include(get_template_directory() .'/template-parts/newBlocks/logo_slider.php');
-        }  elseif( get_row_layout() == 'hotspot_image'){
-            include(get_template_directory() .'/template-parts/newBlocks/hotspots.php');
-        } elseif( get_row_layout() == 'testimonial'){
-            include(get_template_directory() .'/template-parts/newBlocks/testimonial.php');
-        } elseif( get_row_layout() == 'post_news'){
-            include(get_template_directory() .'/template-parts/newBlocks/post_news.php');
-        } elseif( get_row_layout() == 'post_cpt'){
-            include(get_template_directory() .'/template-parts/newBlocks/post_cpt.php');
-        } elseif( get_row_layout() == 'hotspots'){
-            include(get_template_directory() .'/template-parts/newBlocks/hotspots.php');
-        } elseif( get_row_layout() == 'parallax'){
-            include(get_template_directory() .'/template-parts/newBlocks/parallax.php');
-        } elseif( get_row_layout() == 'test'){
-            include(get_template_directory() .'/template-parts/newBlocks/test.php');
-        } elseif (get_row_layout() == 'block_button') {
-            include(get_template_directory() .'/template-parts/newBlocks/_block_buttons.php');
-        } elseif (get_row_layout() == 'debug') {
-            include(get_template_directory() .'/template-parts/newBlocks/_block_debug.php');
+        // CHECK FOR NEW BLOCKS //
+        $blocks = ['block_button', 'block_debug', 'block_team'];
+        if (in_array(get_row_layout(), $blocks)) {
+
+            include(get_template_directory() . '/template-parts/newBlocks/_'. get_row_layout() .'.php');
+
+        } else {
+
+            // OLD BLOCK SUPPORT //
+            if (get_row_layout() == 'heading') {
+                include(get_template_directory() . '/template-parts/newBlocks/heading.php');
+            } elseif (get_row_layout() == 'columns') {
+                include(get_template_directory() . '/template-parts/newBlocks/column_text.php');
+            } elseif (get_row_layout() == 'tabs_simple') {
+                include(get_template_directory() . '/template-parts/newBlocks/tabs_simple.php');
+            } elseif (get_row_layout() == 'full_width_banner') {
+                include(get_template_directory() . '/template-parts/newBlocks/full_width_banner.php');
+            } elseif (get_row_layout() == 'media_object') {
+                include(get_template_directory() . '/template-parts/newBlocks/media_object.php');
+            } elseif (get_row_layout() == 'slider_simple') {
+                include(get_template_directory() . '/template-parts/newBlocks/slider_simple.php');
+            } elseif (get_row_layout() == 'alternating_media') {
+                include(get_template_directory() . '/template-parts/newBlocks/alternating_media.php');
+            } elseif (get_row_layout() == 'gallery_simple') {
+                include(get_template_directory() . '/template-parts/newBlocks/gallery_simple.php');
+            } elseif (get_row_layout() == 'pods') {
+                include(get_template_directory() . '/template-parts/newBlocks/pods.php');
+            } elseif (get_row_layout() == 'form') {
+                include(get_template_directory() . '/template-parts/newBlocks/form.php');
+            } elseif (get_row_layout() == 'table') {
+                include(get_template_directory() . '/template-parts/newBlocks/table.php');
+            } elseif (get_row_layout() == 'spacer') {
+                include(get_template_directory() . '/template-parts/newBlocks/spacer.php');
+            } elseif (get_row_layout() == 'sticky_menu') {
+                include(get_template_directory() . '/template-parts/newBlocks/menu_sticky.php');
+            } elseif (get_row_layout() == 'sticky_menu_anchor') {
+                include(get_template_directory() . '/template-parts/newBlocks/menu_sticky_anchor.php');
+            } elseif (get_row_layout() == 'logo_slider') {
+                include(get_template_directory() . '/template-parts/newBlocks/logo_slider.php');
+            } elseif (get_row_layout() == 'hotspot_image') {
+                include(get_template_directory() . '/template-parts/newBlocks/hotspots.php');
+            } elseif (get_row_layout() == 'testimonial') {
+                include(get_template_directory() . '/template-parts/newBlocks/testimonial.php');
+            } elseif (get_row_layout() == 'post_news') {
+                include(get_template_directory() . '/template-parts/newBlocks/post_news.php');
+            } elseif (get_row_layout() == 'post_cpt') {
+                include(get_template_directory() . '/template-parts/newBlocks/post_cpt.php');
+            } elseif (get_row_layout() == 'hotspots') {
+                include(get_template_directory() . '/template-parts/newBlocks/hotspots.php');
+            } elseif (get_row_layout() == 'parallax') {
+                include(get_template_directory() . '/template-parts/newBlocks/parallax.php');
+            } elseif (get_row_layout() == 'test') {
+                include(get_template_directory() . '/template-parts/newBlocks/test.php');
+            }
         }
 
         $previousBg = $bgColor;
