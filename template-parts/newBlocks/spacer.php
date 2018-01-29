c1a7b63028a89d6c81ab4ee918b11e4b76490ab9
Joe Curran <joe.curran@motionlab.co.uk>
2018-01-29 14:17:20 +0000

    Added Styling CSS
    Added CSS Classes for “hr” elements. Also added option for empty space sperator.

--- template-parts/newBlocks/spacer.php
+++ template-parts/newBlocks/spacer.php
@@ -1,6 +1,18 @@
-<!-- spacer -->
+<?php
+/**
+ * SPACER BLOCK -------------------------------------
+ * Adds a space between blocks.
+ *
+ * @author Jason
+ * @created Unknown
+ * @updated 29 Jan 2018 – Added blank option.
+ *
+ * @version 1.01
+ */
+?>
+
 <section class="relative z1 overflow-hidden || <?php echo $bgColor ?>">
     <div class="container">
-        <hr class="border-<?php echo get_field('border-style'); ?> mt<?php echo get_sub_field('line_space')?> pb<?php echo get_sub_field('line_space')?>">
+        <hr class="border-<?php echo get_sub_field('border_style'); ?> my<?php echo get_sub_field('line_space')?>">
     </div>
 </section>
