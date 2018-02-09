39be329a7963dbfab0d4f31efcf6cd9bca1414de
Joe Curran <joe.curran@motionlab.co.uk>
2018-02-09 10:32:01 +0000

    Added Code Block under Utilites

---
+++ template-parts/newBlocks/utilities/__code.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * UTILITY – BASIC CODE BLOCK ------------------------
+ * A basic block that holds code.
+ *
+ * @author Joe Curran
+ * @created 9 Fab 2018
+ *
+ * @version 1.00
+ */
+
+?>
+
+<section class="utilities-code">
+
+    <?= get_sub_field($current . '_code')?>
+
+</section>
