ae17a90320c77c95a58230f077bd2e3e2117e795
Joe Curran <joe.curran@motionlab.co.uk>
2018-02-09 16:10:54 +0000

    Updated Header BLock to V2
    Updated the header block to version 2 blocks with new block settings.

---
+++ template-parts/newBlocks/_block_heading.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * HEADER BLOCKS --------------------------------------
+ * This is a simple block that allow you to add a
+ * new header to the page.
+ *
+ * @author Joe Curran
+ * @created 9 Feb 2018
+ *
+ * @version 1.00
+ */
+
+switch ($block['layout']):
+    default:
+        include('headers/__basic.php');
+        break;
+endswitch;
---
+++ template-parts/newBlocks/headers/__basic.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * HEADER BLOCKS ---------------------------------------
+ * This is a simple block that allow you to add a
+ * new header to the page.
+ *
+ * @author Joe Curran
+ * @created 9 Feb 2018
+ *
+ * @version 1.00
+ */
+
+?>
+<section class="clearfix <?=$block['spacing']?> <?=$block['padding']?> <?=$block['background']['colour']?> <?=$block['border']['sides']?> <?=$block['border']['size']?> <?=$block['border']['colour']?>">
+
+    <?=($block['grid'] == 'container')? '<div class="container">' : ""?>
+
+        <div class="<?=get_sub_field($current . '_alignment_align')?>">
+
+            <?php
+
+                $order = [];
+                $a = get_sub_field($current . '_subPosition');
+
+                if ($a == "before"):
+                    $order[0] = $current . "_titleSecondly_title";
+                    $order[1] = $current . "_title_title";
+                else:
+                    $order[0] = $current . "_title_title";
+                    $order[1] = $current . "_titleSecondly_title";
+                endif;
+
+            $blockTitle = get_sub_field($order[0]);
+            if (!empty($blockTitle[0]['title'])) {
+                include(BLOCKS_DIR . 'sub-elements/_block_titles.php'); }
+
+            $blockTitle = get_sub_field($order[1]);
+            if (!empty($blockTitle[0]['title'])) {
+                include(BLOCKS_DIR . 'sub-elements/_block_titles.php'); } ?>
+
+        </div>
+
+    <?=($block['grid'] == 'container')? '</div>' : ""?>
+
+</section>
