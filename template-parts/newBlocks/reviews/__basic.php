8347722a7d3ab77c8fb25270d8abf38282e0e371
Joe Curran <joe.curran@motionlab.co.uk>
2018-02-16 13:59:08 +0000

    Added Support for Custom Class/ID

--- template-parts/newBlocks/reviews/__basic.php
+++ template-parts/newBlocks/reviews/__basic.php
@@ -13,7 +13,7 @@
 $blockTitle  = get_sub_field($current . '_title_title');
 ?>
 
-<section class="clearfix <?= $block['spacing'] ?> <?= $block['padding'] ?> <?= $block['background']['colour'] ?> <?= $block['border']['sides'] ?> <?= $block['border']['size'] ?> <?= $block['border']['colour'] ?>">
+<section id="<?=$block['custom_id']?>" class="clearfix relative || <?= $block['spacing'] ?> <?= $block['padding'] ?> <?= $block['background']['colour'] ?> <?= $block['border']['sides'] ?> <?= $block['border']['size'] ?> <?= $block['border']['colour'] ?> <?=$block['custom_css']?>">
 
     <?= ($block['grid'] == 'container') ? '<div class="container">' : "" ?>
 
