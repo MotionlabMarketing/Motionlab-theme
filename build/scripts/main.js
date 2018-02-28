7251f6e0ab46cbc56b2e2cd74c1ed8d496288870
Joe Curran <joe.curran@motionlab.co.uk>
2018-02-28 09:47:38 +0000

    Updated Header Scroll Smallification

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -515,7 +515,7 @@ jQuery(document).ready(function ($) {
     $('a[href*=\\#]:not([href=\\#]):not([data-toggle])').on('click', function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
-            var headerHeight = $('#masthead').outerHeight();
+            var headerHeight = $('header').outerHeight();
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html,body').animate({
