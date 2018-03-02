ac36f3cb0ede07cd8b19e60929b3da3818ddb67f
Joe Curran <joe.curran@motionlab.co.uk>
2018-03-02 09:20:33 +0000

    Updated Video Block Background Colour

--- template-parts/newBlocks/video/__video_stories.php
+++ template-parts/newBlocks/video/__video_stories.php
@@ -110,7 +110,7 @@ endforeach; ?>
 
         <?php if ($block['bgImage']['enable']  && $block['grid'] == 'container'): ?>
 
-            <div class="absolute top-0 left-0 width-100 height-100 bg-center bg-cover zn1 <?=$block['bgImage']['tint']?> <?=$block['bgImage']['tintStrength']?> bg-<?=$block['bgImage']['occupancy']?>" style="background-image: url('<?=$block['bgImage']['image']['url']?>')"></div>
+            <div class="absolute top-0 left-0 width-100 height-100 bg-center bg-cover zn1 <?=$block['bgImage']['tint']?> <?=$block['bgImage']['tintStrength']?> <?=$block['bgImage']['occupancy']?>" style="background-image: url('<?=$block['bgImage']['image']['url']?>')"></div>
 
         <?php endif; ?>
 
@@ -120,12 +120,8 @@ endforeach; ?>
 
     <?php if ($block['grid'] == 'full_width' && $block['bgImage']['enable']): ?>
 
-        <div class="absolute top-0 left-0 width-100 height-100 bg-center bg-cover zn1 <?=$block['bgImage']['tint']?> <?=$block['bgImage']['tintStrength']?> bg-<?=$block['bgImage']['occupancy']?>" style="background-image: url('<?=$block['bgImage']['image']['url']?>')"></div>
+        <div class="absolute top-0 left-0 width-100 height-100 bg-center bg-cover zn1 <?=$block['bgImage']['tint']?> <?=$block['bgImage']['tintStrength']?> <?=$block['bgImage']['occupancy']?>" style="background-image: url('<?=$block['bgImage']['image']['url']?>')"></div>
 
     <?php endif; ?>
 
 </section>
-
-<?php if ($block['bgImage']['enable']): ?>
-
-<?php endif; ?>
