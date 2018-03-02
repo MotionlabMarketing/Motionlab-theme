8317d221ded1902e25fdaf8282911a398f07150c
Joe Curran <joe.curran@motionlab.co.uk>
2018-03-02 10:08:51 +0000

    Added New Large CTA Block

--- template-parts/newBlocks/_block_cta.php
+++ template-parts/newBlocks/_block_cta.php
@@ -22,6 +22,7 @@ endif;
 // BLOCK CONTENT
 $block['content']['title']    = get_sub_field($current . '_title_title');
 $block['content']['content']  = get_sub_field($current . '_content');
+$block['content']['preline']  = get_sub_field($current . '_preLink');
 $block['content']['link']     = get_sub_field($current . '_link');
 $block['content']['buttons']  = get_sub_field($current . '_buttons');
 
@@ -29,6 +30,9 @@ $block['content']['txtColor'] = get_sub_field($current . '_txtColour_system_text
 $block['content']['txtAlign'] = get_sub_field($current . '_txtAlign');
 
 switch ($block['layout']):
+    case "large":
+        include('cta/__large.php');
+        break;
     case "sections":
         include('cta/__sections.php');
         break;
