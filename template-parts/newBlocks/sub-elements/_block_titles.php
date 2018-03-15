b501611ac4e04d74ca7ecbd1d19a76234e1018d2
Joe Curran <joe.curran@motionlab.co.uk>
2018-03-15 15:23:37 +0000

    Fixed Bug with Title

--- template-parts/newBlocks/sub-elements/_block_titles.php
+++ template-parts/newBlocks/sub-elements/_block_titles.php
@@ -8,6 +8,7 @@
  *
  * @version 1.00
  */
+$block['block_title'] = $blockTitle;
 ?>
 
 <<?=$block['block_title'][0]['type']['heading']?> class="pb2 <?=$block['block_title'][0]['size']['heading_size']?> || <?=$block['block_title'][0]['color']['system_text_colours']?> <?=$block['block_title'][0]['title_case']['system_text_transform']?>">
