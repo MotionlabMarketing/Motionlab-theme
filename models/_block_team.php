943170c26815d6a0f7cce02746657b8069f212f7
Karl Fletcher <karl@motionlab.co.uk>
2018-04-04 16:01:34 +0100

    Ref T8060 - Added ability to select team members that will show in block

--- models/_block_team.php
+++ models/_block_team.php
@@ -27,20 +27,33 @@ Class _block_team
 
 	private function loadBlockSettings() {
 
-		$this->profile_count          = get_sub_field('block_team_profiles') != "" ? get_sub_field('block_team_profiles') : 5;
-		$this->block['page_button']   = get_sub_field('block_team_button');
+		$this->profile_count            = get_sub_field('block_team_profiles') != "" ? get_sub_field('block_team_profiles') : 5;
+		$this->selected_profiles        = get_sub_field('block_team_selected_profiles');
+		$this->block['page_button']     = get_sub_field('block_team_button');
 
 	}
 
 	public function fetchPosts($profile_count = 5) {
 
-	    $args = array(
-			'posts_per_page'    => $profile_count,
-			'paged'             => $_POST['block_page'] ?: 1,
-			'post_type'         => 'team_members'
-		);
+		$this->block['posts']->posts = [];
+		foreach($this->selected_profiles as $post_id) :
+			$this->block['posts']->posts[] = get_post($post_id);
+		endforeach;
 
-		$this->block['posts'] = new WP_Query( $args );
+		if(empty($this->selected_profiles) || sizeof($this->selected_profiles) < $profile_count) :
+
+			$args = array(
+				'posts_per_page'    => $profile_count - (!empty($this->selected_profiles) ? sizeof($this->selected_profiles) : 0),
+				'paged'             => 1,
+				'post_type'         => 'team_members',
+				'order'             => 'rand',
+				'post__not_in'      => $this->selected_profiles
+			);
+
+			$temp_members = new WP_Query( $args );
+
+			$this->block['posts']->posts = array_merge($this->block['posts']->posts, $temp_members->posts);
+		endif;
 
 		return $this->block['posts'];
 
