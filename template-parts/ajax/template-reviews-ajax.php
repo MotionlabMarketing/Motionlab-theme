96673546ee513e2053f4e87b43fb03f79846304d
Karl Fletcher <karl@motionlab.co.uk>
2018-04-06 15:13:00 +0100

    Ref T8181 - Added reviews template (based on testimonials template) along with helper function

---
+++ template-parts/ajax/template-reviews-ajax.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karl
+ * Date: 05/04/18
+ * Time: 10:52
+ */
+
+?>
+<div class="grid-sizer"></div>
+
+<?php foreach($testimonials['posts']->posts as $post) : ?>
+
+    <div class="col col-12 sm-col-6 md-col-4 p2 mt5 grid-item || text-center">
+
+        <div class="px5 pt5 pb3 bg-smoke">
+
+            <div class="mb4">
+                <?=get_stars(get_field('star_rating'))?>
+            </div>
+
+            <?=get_field('reviewer_body')?>
+
+            <hr class="my4">
+
+            <h3 class="h4 brand-primary text-left mb1"><?=get_field('reviewer_name')?></h3>
+
+            <p class="text-left"><?=get_field('reviewer_locations')?></p>
+
+        </div>
+
+    </div>
+<?php endforeach; ?>
