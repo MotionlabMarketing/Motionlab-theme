cbcde58ee12edd0a21070499769c0fbed27b702f
Karl Fletcher <karl@motionlab.co.uk>
2018-04-18 11:56:29 +0100

    Ref T8346 - Replaced standard reviews block with MVC variant

--- template-parts/newBlocks/reviews/__basic.php
+++ template-parts/newBlocks/reviews/__basic.php
@@ -10,59 +10,50 @@
  * @version 1.00
  */
 
-$blockTitle  = get_sub_field($current . '_title_title');
+$blockTitle  = $block['title'];
+
 ?>
 
 <section <?=get_blockID($block)?> <?=get_blockClasses($block, "")?> <?=get_blockData($block)?>>
 
     <?= ($block['grid'] == 'container') ? '<div class="container">' : "" ?>
 
-        <?php  if (!empty($blockTitle[0]['title']) || !empty(get_sub_field('block_pods_content'))): ?>
-        <div class="m4 mb5 || text-center">
+    <?php  if (!empty($blockTitle[0]['title']) || !empty(get_sub_field('block_pods_content'))): ?>
+    <div class="m4 mb5 || text-center">
 
-            <div class="mb3">
-                <?php
-                if (!empty($blockTitle[0]['title'])) {
-                    include(get_template_directory() .'/template-parts/newBlocks/sub-elements/_block_titles.php'); } ?>
-            </div>
-
-            <div class="text-center limit-p limit-p-80">
-                <?=get_sub_field('block_pods_content')?>
-            </div>
-
+        <div class="mb3">
+            <?php
+            if (!empty($blockTitle[0]['title'])) {
+                include(get_template_directory() .'/template-parts/newBlocks/sub-elements/_block_titles.php'); } ?>
         </div>
-        <?php endif; ?>
 
-        <?php
-        $args = array(
-            'post_status' => 'publish',
-            'posts_per_page' => 3,
-            'post_type' => 'reviews'
-        );
-        $query = new WP_Query($args);
-        if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+        <div class="text-center limit-p limit-p-80">
+            <?=get_sub_field('block_pods_content')?>
+        </div>
 
+    </div>
+    <?php endif; ?>
+
+    <?php foreach($block['posts']->posts as $post) : ?>
             <div class="col col-12 md-col-4 text-center">
 
                 <div class="p3">
 
                     <div class="mb4">
-                        <?= get_stars(get_field('star_rating')) ?>
+                        <?= get_stars(get_field('star_rating', $post->ID)) ?>
                     </div>
 
                     <div class="wysiwyg mb3 mx5">
-                        <?= get_field('reviewer_body'); ?>
+                        <?= get_field('reviewer_body', $post->ID); ?>
                     </div>
 
-                    <h4 class="h4 mb0 brand-primary bold"><?= get_field('reviewer_name') ?></h4>
-                    <p class="h4 brand-primary"><?= get_field('reviewer_locations') ?></p>
+                    <h4 class="h4 mb0 brand-primary bold"><?= get_field('reviewer_name', $post->ID) ?></h4>
+                    <p class="h4 brand-primary"><?= get_field('reviewer_locations', $post->ID) ?></p>
 
                 </div>
 
             </div>
-
-        <?php endwhile; endif;
-        wp_reset_postdata(); ?>
+    <?php endforeach; ?>
 
     <?= ($block['grid'] == 'container') ? '</div>' : "" ?>
 
