2af831bbd1c6bbfec05462e7ea155fe4afe962a4
Karl Fletcher <karl@motionlab.co.uk>
2018-04-30 09:40:05 +0100

    Ref T8477 - Added caravan sorting functionality and update pagination block

--- models/_caravans.php
+++ models/_caravans.php
@@ -8,8 +8,8 @@
 Class _caravans extends CPTCore
 {
 
-	public $enabled = true;
-	private $block = [];
+	public $enabled         = true;
+	private $block          = [];
 	private $makes_slug     = 'makes';
 	private $specs_slug     = 'specs';
 
@@ -22,9 +22,9 @@ Class _caravans extends CPTCore
 
 	private function getCategories() {
 
-		$make_terms                         = get_terms($this->makes_slug);
-		$specs_terms                        = get_terms($this->specs_slug);
-		$berth_options                      = $this->get_distinct_meta_values('caravan_details_berth');
+		$make_terms                             = get_terms($this->makes_slug);
+		$specs_terms                            = get_terms($this->specs_slug);
+		$berth_options                          = $this->get_distinct_meta_values('caravan_details_berth');
 
 		$this->block['make_select_terms']       = $make_terms;
 		$this->block['specs_select_terms']      = $specs_terms;
@@ -34,20 +34,48 @@ Class _caravans extends CPTCore
 
 	private function fetchCaravans() {
 
-		$tax_query = [];
+		$tax_query      = [];
+		$orderby_query  = [];
+
+		if(isset($_POST['sortby_age']) && $_POST['sortby_age'] != '') :
+			$orderby_query['age_clause']    = $_POST['sortby_age'];
+		endif;
+
+		if(isset($_POST['sortby_price']) && $_POST['sortby_price'] != '') :
+			$orderby_query['price_clause']  = $_POST['sortby_price'];
+		endif;
+
+		if(isset($_POST['sortby_berth']) && $_POST['sortby_berth'] != '') :
+			$orderby_query['berth_clause']  = $_POST['sortby_berth'];
+		endif;
 
 		$args = array(
 			'posts_per_page'    => 8,
 			'paged'             => $_POST['page_number'] ?: 1,
 			'post_type'         => 'caravans',
-			'orderby'           => 'date',
-			'order'             => 'DESC',
+			'meta_query'        => array(
+	            'relation'      => 'AND',
+	            'berth_clause'  => array(
+	                'key'           => 'caravan_details_berth',
+	                'compare'       => 'EXISTS',
+	            ),
+	            'age_clause'    => array(
+	                'key'           => 'caravan_details_build_year',
+	                'compare'       => 'EXISTS',
+	            ),
+	            'price_clause'  => array(
+	                'key'           => 'caravan_details_price',
+	                'compare'       => 'EXISTS',
+	            )
+	        ),
+			'orderby'           => $orderby_query,
 			'post_status'       => array('publish'),
 			'tax_query'         => $tax_query
 		);
 
 		$caravans = new WP_Query($args);
 
+		//Fetch gallery images for this caravan.
 		foreach($caravans->posts as $post) :
 			$collection_id = get_field('caravan_details_showcase', $post->ID);
 
@@ -66,7 +94,6 @@ Class _caravans extends CPTCore
 				$image = new WP_Query($showcase_args);
 				$post->showcase_image = $image;
 			endif;
-
 		endforeach;
 
 		$this->block['posts'] = $caravans;
