69f8aa9f31b17c3fd7549dafcda79a2fbd971488
Joe Curran <joe.curran@motionlab.co.uk>
2018-05-03 14:13:25 +0100

    Added Image Resizer and Cropping

--- front-page.php
+++ front-page.php
@@ -1,9 +1,8 @@
 <?php
 get_header();
-?>
-<?php while ( have_posts() ) : the_post();?>
+while ( have_posts() ) : the_post();
 
-	<?php include(get_template_directory() .'/template-parts/building-blocks.php' ); ?>
+	include(get_template_directory() .'/template-parts/building-blocks.php' );
 
-<?php endwhile; ?>
-<?php get_footer();?>
+endwhile;
+get_footer();
