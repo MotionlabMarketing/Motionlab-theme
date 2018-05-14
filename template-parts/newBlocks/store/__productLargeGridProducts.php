c2e84a21787b64416eda8b0db93ebf05ab5adc18
Karl Fletcher <karl@motionlab.co.uk>
2018-05-14 13:43:56 +0100

    Ref T8536 - Updated product pods block to pull from database.

--- template-parts/newBlocks/store/__productLargeGridProducts.php
+++ template-parts/newBlocks/store/__productLargeGridProducts.php
@@ -13,6 +13,10 @@
 
 // TODO: BUG: Match Height has a problem with the second tab matching.
 
+$selected_products = get_sub_field('block_store_products');
+
+pa($selected_products);
+
 ?>
 
 <section <?=get_blockID($block)?> <?=get_blockClasses($block, "")?> <?=get_blockData($block)?>>
@@ -29,9 +33,9 @@
 
                     <div data-element="tabs" class="col col-12 sm-flex items-center justify-center mb5">
 
-                        <?php $i = 1; foreach ($block['tabs'] as $tab): ?>
+                        <?php $i = 0; foreach ($block['tabs'] as $tab): ?>
 
-                            <span data-section="tab<?=$i?>" class="block text-center bold bg-smoke border-top border-left border-bottom <?=(count($block['tabs']) == $i)? ' border-right':''?> border-light cursor-pointer py2 px5 relative <?=($i <= 1)? 'tab-active' : '' ?>">
+                            <span data-section="tab<?=$i?>" class="block text-center bold bg-smoke border-top border-left border-bottom <?=(count($block['tabs']) == $i)? ' border-right':''?> border-light cursor-pointer py2 px5 relative <?=($i <= 0)? 'tab-active' : '' ?>">
                                 <?=$tab['category_title']?>
                             </span>
 
@@ -41,9 +45,20 @@
 
                     <div class="clearfix" data-tabs="content">
 
-                        <?php $i = 1; foreach ($block['tabs'] as $tab): ?>
+                        <?php $i = 0; foreach ($block['tabs'] as $tab): ?>
 
-                            <section id="tab<?=$i?>" class="clearfix <?=($i > 1)? 'hide' : '' ?>" style="max-height: 70vh">
+                            <section id="tab<?=$i?>" class="clearfix <?=($i > 0)? 'hide' : '' ?>" style="max-height: 70vh">
+
+                                <?php
+
+                                    //The first product in the list will be the "caravan/motorhome of the month. $i-1 because tabs are indexed at 1 whereas post are at 0"
+                                    $cotm = array_shift($selected_products[$i]['items']);
+                                    $prefix = $cotm->post_type == 'caravans' ? 'caravan' : 'motorhome';
+
+                                    $title  = get_the_title($cotm->ID);
+                                    $price  = get_field($prefix.'_details_price', $cotm->ID);
+                                    $specs  = array_slice(get_field($prefix.'_details_specifications', $cotm->ID), 0, 4);
+                                ?>
 
                                 <div class="col col-12 md-col-5 relative" data-mh="product-size-tab<?=$i?>">
 
@@ -55,21 +70,24 @@
                                                 <?=render_attachment_image("8199", "large", false,  ["class" => "mb4"])?> <?php // NEED ID REPLACING WITH ID FOR IMAGE.  ?>
                                             </div>
 
-                                            <h3>Challenger</h3>
+                                            <h3><?=$title?></h3>
 
-                                            <ul class="list-reset border-last-right-none">
-                                                <li class="inline border-right px2">Item 1</li>
-                                                <li class="inline border-right px2">Item 2</li>
-                                                <li class="inline border-right px2">Item 3</li>
-                                                <li class="inline border-right px2">Item 4</li>
-                                            </ul>
+                                            <?php if (!empty($specs)): ?>
 
-                                            <p><strong><span class="brand-primary h2">£18,685</span></strong></p>
+                                              <ul class="list-reset border-last-right-none">
+                                                  <?php foreach($specs as $spec) : ?>
+                                                      <li class="inline border-right px2"><?=get_term($spec)->name?></li>
+                                                  <?php endforeach; ?>
+                                              </ul>
+
+                                            <?php endif; ?>
+
+                                            <p><strong><span class="brand-primary h2">£<?=number_format($price)?></span></strong></p>
 
                                             <?php
 
-                                            $button['button_link']['url']        = "#";
-                                            $button['button_link']['title']      = "View Range";
+                                            $button['button_link']['url']        = get_permalink($cotm->ID);
+                                            $button['button_link']['title']      = "View " . ucwords($prefix);
                                             $button['system_text_colours']       = "white";
                                             $button['system_background_colours'] = "transparent";
 
@@ -83,28 +101,36 @@
 
                                 <div class="col col-12 md-col-7 mrn2" data-mh="product-size-tab<?=$i?>" style="max-height: 70vh">
 
-                                    <?php $a = 0; while($a < 6): ?>
+                                    <?php foreach($selected_products[$i]['items'] as $selected_product): ?>
+
+                                        <?php
+                                            $prefix = $selected_product->post_type == 'caravans' ? 'caravan' : 'motorhome';
+
+                                            $title  = get_the_title($selected_product->ID);
+                                            $price  = get_field($prefix.'_details_price', $selected_product->ID);
+                                            $berth  = get_field($prefix.'_details_berth', $selected_product->ID);
+                                        ?>
 
                                         <div class="col col-12 sm-col-6 md-col-4 pl3 pr3 mb5">
 
                                             <div class="product-item col p4 text-center box-shadow-2" data-mh="product-item">
 
-                                                <h3 class="h4 bold">Challenger</h3>
+                                                <h3 class="h4 bold"><?=$title?></h3>
 
                                                 <div class="flex items-center justify-center" data-mh="product-item-image">
                                                     <?=render_attachment_image("8199", "medium", false,  ["class" => "mb4"])?> <?php // NEED ID REPLACING WITH ID FOR IMAGE.  ?>
                                                 </div>
 
-                                                <p class="mb2 bold">2 Berth</p>
+                                                <p class="mb2 bold"><?=$berth?> Berth</p>
 
-                                                <p><strong><span class="brand-primary" data-element="price" style="font-size: 1.2rem">£18,685</span></strong></p>
+                                                <p><strong><span class="brand-primary" data-element="price" style="font-size: 1.2rem">£<?=number_format($price)?></span></strong></p>
 
                                                 <?php
 
                                                     // NEEDS TO BE ABLE TO THE CHANGE THE VALUE FROM 'RANGE' OR 'CARAVAN/MOTORHOME' //
 
-                                                    $button['button_link']['url']        = "#";
-                                                    $button['button_link']['title']      = "View Range";
+                                                    $button['button_link']['url']        = get_permalink($selected_product->ID);
+                                                    $button['button_link']['title']      = "View " . ucwords($prefix);
                                                     $button['system_text_colours']       = "white";
                                                     $button['system_background_colours'] = "transparent";
 
@@ -115,8 +141,7 @@
 
                                         </div>
 
-                                    <?php $a++; endwhile; ?>
-
+                                    <?php endforeach; ?>
                                 </div>
 
                             </section>
