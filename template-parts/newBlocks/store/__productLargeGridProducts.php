6597578daf1ce0bef87c4a50dfdb1a96b6f76f5b
Joe Curran <joe.curran@motionlab.co.uk>
2018-05-14 16:35:03 +0100

    Bug Fix for Karl & Image Update

--- template-parts/newBlocks/store/__productLargeGridProducts.php
+++ template-parts/newBlocks/store/__productLargeGridProducts.php
@@ -55,14 +55,15 @@ $selected_products = get_sub_field('block_store_products');
 
                                     $title  = get_the_title($cotm->ID);
                                     $price  = get_field($prefix.'_details_price', $cotm->ID);
-                                    $specs  = array_slice(get_field($prefix.'_details_specifications', $cotm->ID), 0, 4);
+                                    if (is_array($prefix.'_details_specifications'))
+                                        $specs  = array_slice(get_field($prefix.'_details_specifications', $cotm->ID), 0, 4);
                                 ?>
 
                                 <div class="col col-12 md-col-5 relative" data-mh="product-size-tab<?=$i?>">
 
                                         <div class="product-item p4 mr3 text-center mb4 ml2 box-shadow-2">
 
-                                            <?=render_attachment_image("8200", ['180', '300'], false,  ["class" => "mt5 mx-auto"])?> <?php // NEED ID REPLACING WITH ID FOR IMAGE.  ?>
+                                            <?=render_attachment_image("8200", ['180', '300'], false,  ["class" => "mt5 mb5 mx-auto"])?> <?php // NEED ID REPLACING WITH ID FOR IMAGE.  ?>
 
                                             <div class="flex items-center justify-center">
                                                 <?=render_attachment_image("8199", "large", false,  ["class" => "mb4"])?> <?php // NEED ID REPLACING WITH ID FOR IMAGE.  ?>
@@ -113,10 +114,10 @@ $selected_products = get_sub_field('block_store_products');
 
                                             <div class="product-item col p4 text-center box-shadow-2" data-mh="product-item">
 
-                                                <h3 class="h4 bold"><?=$title?></h3>
+                                                <h3 class="h4 bold" data-mh="<?=$block['id']?>-title"><?=$title?></h3>
 
                                                 <div class="flex items-center justify-center" data-mh="product-item-image">
-                                                    <?=render_attachment_image("8199", "medium", false,  ["class" => "mb4"])?> <?php // NEED ID REPLACING WITH ID FOR IMAGE.  ?>
+                                                    <?=render_attachment_image("8199", "medium", false,  ["class" => ""])?> <?php // NEED ID REPLACING WITH ID FOR IMAGE.  ?>
                                                 </div>
 
                                                 <p class="mb2 bold"><?=$berth?> Berth</p>
@@ -126,13 +127,13 @@ $selected_products = get_sub_field('block_store_products');
                                                 <?php
 
                                                     // NEEDS TO BE ABLE TO THE CHANGE THE VALUE FROM 'RANGE' OR 'CARAVAN/MOTORHOME' //
-
                                                     $button['button_link']['url']        = get_permalink($selected_product->ID);
                                                     $button['button_link']['title']      = "View " . ucwords($prefix);
                                                     $button['system_text_colours']       = "white";
                                                     $button['system_background_colours'] = "transparent";
 
                                                     ?>
+
                                                 <?php render_button($button, "medium", ["class" => "bold bg-brand-secondary hover-white py1 px2"]) ?>
 
                                             </div>
