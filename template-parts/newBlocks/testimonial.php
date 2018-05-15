ffa9a6b02cab75237f77e3a7dec36df56e5d3b0c
Joe Curran <joe.curran@motionlab.co.uk>
2018-05-15 15:12:43 +0100

    T8544 - Timeline Block Settings Errors

--- template-parts/newBlocks/testimonial.php
+++
@@ -1,7 +0,0 @@
-<!-- testimonial -->
-<div class="container <?php echo $masterPad ;?> my6">
-    <blockquote cite="" class="lsn1 lh1 h2 lg-h1 m0 mb4 bold">
-        <?php echo get_sub_field('testimonial'); ?>
-    </blockquote>
-    <cite class="bold"><?php echo get_sub_field('subtitle'); ?></cite>
-</div>
