9e5d315783abbe2fce68ea7826ac92c77a3ea0f1
Joe Curran <joe.curran@motionlab.co.uk>
2018-06-07 15:24:22 +0100

    Ref T9236: Template Introduction File Inclusion

--- templates/_parts/__introductions.php
+++ templates/_parts/__introductions.php
@@ -1,30 +1,31 @@
 <?php
-    // GET THE VALUES READY.
+    // TODO: REVIEW THE USE OF THIS FILE...
 
-    $template_title   = get_field('page_title');
-    $template_content = get_field('page_introduction');
-
-    $blockTitle = $template_title['title'];
-
+    // $template_title   = get_field('page_title')['title'];
+    // $blockTitle = $template_title['title'];
+    
+    $heading = convert_heading(get_field('page_title')['title']);
+    $intro   = get_field('page_introduction');
 ?>
 
-<?php if (!empty($blockTitle[0]['title']) || !empty($template_content)): ?>
+<?php if (!empty($heading) || !empty($intro)): ?>
 
     <div class="container clearfix">
 
-        <div class="col-12 || mb5 || text-center">
+        <div class="col-12 mb5 text-center">
 
-            <div class="col col-12 md-col-12 lg-col-12 || mb5 text-center">
+            <div class="col col-12 md-col-12 lg-col-12 mb5 text-center">
 
-                <?php if (!empty($blockTitle[0]['title'])): ?>
-                    <div class="mb4">
-                        <?php include(get_template_directory() .'/template-parts/newBlocks/sub-elements/_block_titles.php');?>
+                <?php if (!empty($heading)): ?>
+                    <div class="mb3">
+                        <?php render_heading("{$heading->title}", "{$heading->type}", "{$heading->size}", "{$heading->color}", "{$heading->case}"); ?>
+                        <?php // include(get_template_directory() .'/template-parts/newBlocks/sub-elements/_block_titles.php');?>
                     </div>
                 <?php endif; ?>
 
-                <?php if (!empty($template_content)): ?>
+                <?php if (!empty($intro)): ?>
                     <div class="wysiwyg h4 limit-p limit-p-70">
-                        <?=$template_content?>
+                        <?=$intro?>
                     </div>
                 <?php endif; ?>
 
