724b7bc10105a2f10aba3475931d9b869a7bb572
Joe Curran <joe.curran@motionlab.co.uk>
2018-07-17 15:56:20 +0100

    Updated the title below link block

--- template-parts/newBlocks/linkedBoxes/__title_below.php
+++ template-parts/newBlocks/linkedBoxes/__title_below.php
@@ -13,7 +13,6 @@
  // TODO: Need updating to new system and all support checking.
 
 $bgColor          = get_sub_field($current . '_background_system_background_colours');
-$txtColor         = get_sub_field($current . '_text_system_text_colours');
 $block['content']['txtAlign'] = get_sub_field('block_linkBoxes_txtAlign_align');
 
 $blockTitle       = get_sub_field($current . '_title_title');
@@ -51,7 +50,7 @@ $block['content']['button'] = get_sub_field($current . '_button_button_link');
                                     <iframe width="100%" height="270" src="https://www.youtube.com/embed/<?=$video['id'][1]?>" class="js-match-height" frameborder="0" allow="encrypted-media" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                                 </div>
                             <?php else: ?>
-                                <div class="image-holder js-match-height bg-grey <?= $txtColor ?>"
+                                <div class="image-holder js-match-height bg-grey"
                                  style="background-image: url('<?=$item['block_linkBoxes_image']?>')"></div>
                             <?php endif; ?>
                             <div class="flex items-center justify-center p3 bold bg-white <?=$item['block_linkBoxes_button']['system_text_colours']?>" data-mh="box-title">
