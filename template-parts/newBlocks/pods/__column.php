e7ddedac2692ba5d137f6e2f2b12034920b3ed7c
Karl Fletcher <karl@motionlab.co.uk>
2018-07-18 10:50:38 +0100

    Ref T9849 - Updated functions.php with new image size and changes pods column template to use new image size

--- template-parts/newBlocks/pods/__column.php
+++ template-parts/newBlocks/pods/__column.php
@@ -30,7 +30,7 @@
 
                                 <?=(!empty($item['button']['button_link']['url'])? '<a href="'. $item['button']['button_link']['url'] .'" class="block overflow-hidden hover-zoom">' : "")?>
 
-                                    <?=wp_get_attachment_image($item['image'],'galleryMedium js-match-height')?>
+                                    <?=wp_get_attachment_image($item['image'], "linkedBoxes", "", ["data-mh" => "{$block['id']}-image"])?>
 
                                 <?=(!empty($item['button']['button_link']['url'])? '</a>' : "")?>
 
