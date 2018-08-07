e452da2a7a950aebda3ab97efc3adf6d24105dd2
Joe Curran <joe.curran@motionlab.co.uk>
2018-08-07 09:25:44 +0100

    Removed Unused File
    Updated logos to include links.

--- template-parts/newBlocks/_block_logos.php
+++ template-parts/newBlocks/_block_logos.php
@@ -17,9 +17,6 @@ $block['logos']   = get_sub_field($current . '_items');
 $blockTitle = $block['title']; // REPLACE WHEN HEADER UPDATED.
 
 switch ($block['layout']):
-    case "partners":
-        include('logos/__partners.php');
-        break;
     default:
         include('logos/__basic.php');
         break;
--- template-parts/newBlocks/logos/__basic.php
+++ template-parts/newBlocks/logos/__basic.php
@@ -1,38 +1,83 @@
 <?php
 /**
- * LOGO BASICS BLOCKS ---------------------------------------
- * This block add logos to the page in a single line that
- * will auto scroll if needed.
- *
- * @author Joe Curran
- * @created 28 Feb 2018
- *
- * @version 2.00
- */
+* LINKED BOXES – BASIC LAYOUT BLOCK ------------------------
+* A basic block which allows images to be linked to other
+* pages or URLs.
+*
+* @author Joe Curran
+* @created 29 Jan 2018
+*
+* @version 1.00
+*/
+
+$bgColor    = get_sub_field($current . '_background_system_background_colours');
+$txtColor   = get_sub_field($current . '_text_system_text_colours');
+
+$borders          = "";
+$bordersColor     = get_sub_field($current . '_borders_border_colour');
+$bordersSides     = get_sub_field($current . '_borders_border_sides');
+
+if (!empty($bordersSides)) {
+    foreach ($bordersSides as $item) {
+        $borders = $borders . " " . $item;
+    }
+}
+$borders    = $borders . " " . $bordersColor;
+
+$blockTitle = get_sub_field($current . '_title_title');
+
+$blockItems = get_sub_field($current . '_slider');
+$blockItems = $blockItems['logos'];
+?>
+
+<?php
+/**
+* LOGO BASICS BLOCKS ---------------------------------------
+* This block add logos to the page in a single line that
+* will auto scroll if needed.
+*
+* @author Joe Curran
+* @created 28 Feb 2018
+*
+* @version 2.00
+*/
 ?>
 
 <section <?=get_blockID($block)?> <?=get_blockClasses($block, "")?> <?=get_blockData($block)?>>
-
+    
     <?= ($block['grid'] == 'container') ? '<div class="container">' : "" ?>
-
+        
         <?php include(BLOCKS_DIR . '_parts/__basic_introduction.php'); ?>
 
-        <div class="item-slider md-pl5" data-slick="logo-slider">&nbsp;<?php // NEEDED TO KEEP BOX HEIGHT. ?>
+        <div class="col-12">
+            
+            <?php if (!empty($block['logos'])): ?>
 
-            <?php foreach ($block['logos'] as $logo): ?>
+                <div class="item-slider px5" data-slick="logo-slider-partners">&nbsp;
+                    
+                    <?php foreach ((array)$block['logos'] as $logo): $link = get_field("link", $logo['ID']);?>
 
-                <div class="logo px2 js-match-height">
-
-                    <div class="flex items-center height-100">
-                    <?=wp_get_attachment_image($logo['ID'], array(160, 160), "", ["class" => "block mx-auto"])?>
-                    </div>
-
+                        <div class="col col-grid-5 flex items-center justify-center px5" data-mh="partners-slider">
+                            
+                            <?php if(!empty($link['url'])): ?><a href="<?=$link['url']?>" <?=($link['title'])? 'title="'.$link['title'].'"':""?> <?=($link['target'])? 'target="'.$link['target'].'"':""?>><?php endif; ?>
+                                <img src="<?=$logo['url']?>" alt="<?=$logo['title']?>" style="max-height: 6rem; margin: 0 auto;">
+                            <?php if(!empty($link['url'])): ?></a><?php endif; ?>
+                                
+                        </div>
+                            
+                    <?php endforeach; ?>
+                        
                 </div>
 
-            <?php endforeach; ?>
+            <?php else: ?>
 
+                <p class="lead text-center"><strong>LOGO BLOCK</strong><br/>Please select or upload some logos into this block!</p>
+            
+            <?php endif; ?>
+                    
         </div>
-
+            
     <?= ($block['grid'] == 'container') ? '</div>' : "" ?>
-
+            
 </section>
+        
