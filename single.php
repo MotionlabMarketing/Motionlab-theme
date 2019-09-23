1d18bfa697b221418af249934b60159766ff5526
Joe Curran <joe.curran@motionlab.co.uk>
2019-09-23 11:05:24 +0100

    Stu's fix for post thumbnails

--- single.php
+++ single.php
@@ -31,7 +31,7 @@ $taxonomies = get_the_terms(get_the_ID(), 'category');
                     <p class="left mt1 h5"><?=ml_time_elapsed_string(get_the_date())?></p>
                     
                     <ul class="mt2 tags tags-right border-radius">
-                        <?php if (is_array($taxonomies)): foreach($taxonomies as $category) :?>
+                        <?php if (is_array($taxonomies)): foreach ($taxonomies as $category) :?>
                             <li><a href="/news/<?=$category->slug?>"><?=$category->name?></a></li>
                         <?php endforeach; endif; ?>
                     </ul>
@@ -54,7 +54,7 @@ $taxonomies = get_the_terms(get_the_ID(), 'category');
         
         
         <?php
-        $related = get_posts( array(
+        $related = get_posts(array(
             'category__in' => wp_get_post_categories($post->ID),
             'numberposts' => 4,
             'post__not_in' => array($post->ID),
@@ -69,7 +69,13 @@ $taxonomies = get_the_terms(get_the_ID(), 'category');
             
             <div class="mxn3 clearfix">
                 
-                <?php foreach ($related as $post): ?>
+                <?php foreach ($related as $post):
+                    if (has_post_thumbnail($post->ID)) {
+                        $image_url = get_attachment_image_url(get_post_thumbnail_id($post->ID), 'medium');
+                    } else {
+                        $image_url = get_field('fallback_image_news_listing', 'option');
+                    }
+                    ?>
                     
                     <div class="relative col col-12 sm-col-6 md-col-3 lg-col-3 pt4 pl4 pr4 mb5" data-mh="post">
                         
@@ -79,13 +85,13 @@ $taxonomies = get_the_terms(get_the_ID(), 'category');
                         
                         <a href="<?=get_permalink($post->ID)?>"><div class="image-holder square img-cover img-center mb4" style="background-image: url('<?=resize_attachment_image($image_url, 500, 500, true)?>');"></div></a>
                         
-                        <p class="h5 mb3" data-mh="post-content"><?= strlen($post->post_excerpt) > 1 ? strip_tags($post->post_excerpt) : shorten_string(strip_tags($post->post_content),30);?></p>
+                        <p class="h5 mb3" data-mh="post-content"><?= strlen($post->post_excerpt) > 1 ? strip_tags($post->post_excerpt) : shorten_string(strip_tags($post->post_content), 30);?></p>
                         
                         <a href="<?=get_permalink($post->ID)?>" class="block mb4 h5 bold">Read full story</a>
                         
                         <?php $taxonomies = get_the_terms($post->ID, 'category');?>
                         <ul class="tags border-radius">
-                            <?php foreach($taxonomies as $category) :?>
+                            <?php foreach ($taxonomies as $category) :?>
                                 <li><a href="/news/<?=$category->slug?>"><?=$category->name?></a></li>
                             <?php endforeach; ?>
                         </ul>
