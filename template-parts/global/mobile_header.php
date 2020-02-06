4d61dbd6cdcff5dd9b574044deae84f8b5b31b81
Joe Curran <joe.curran@motionlab.co.uk>
2020-02-06 14:45:58 +0000

    Made amendments to theme for mobile menu logic. Allows new logo to appear on work-for-us page

--- template-parts/global/mobile_header.php
+++ template-parts/global/mobile_header.php
@@ -1,6 +1,12 @@
 <div class="p3 bg-brand-primary flex items-center h3 relative z3">
-    <a href="/" class="flex items-center height-100 border-box">
-        <img src="<?php the_field('brand_logo_white', 'options') ?>" alt="" class="block mx-auto width-100" style="max-width:12rem;">
+    <a href="/" class="mobile-logo flex items-center height-100 border-box">
+
+        <?php if(strpos($_SERVER['REQUEST_URI'], '/work-for-us') === FALSE ): ?>
+            <img src="<?php the_field('brand_logo_white', 'options') ?>" alt="" class="block mx-auto width-100" style="max-width:12rem;">
+        <?php else: ?>
+            <img src="<?= get_stylesheet_directory_uri() . "/assets/img/cummins-mellor-family.svg"; ?>" alt="" id="main-logo-family" class="block mx-auto" data-logo="<?= get_stylesheet_directory_uri() . "/assets/img/cummins-mellor-family.svg"; ?>" data-scrolllogo="<?= get_stylesheet_directory_uri() . "/assets/img/cummins-mellor-family.svg"; ?> ">
+        <?php endif; ?>
+
     </a>
     <div class="flex-auto">
         <div class="flex justify-end">
